a4e530afa4b9d9396c13922a9c67358e52ba0e17
Naoise Ryan <c00281427@setu.ie>
2026-01-31 15:02:31 +0000

    Added support for background sprite on menu

--- RatPack/RatPack/Menu.cpp
+++ RatPack/RatPack/Menu.cpp
@@ -5,11 +5,16 @@ Menu::Menu() {
 	//Size Dimensions
 	m_startButtonSize = { 100, 100 };
 	m_startButtonPosition = { 100, 100 };
+	m_menuBgPosition = { 0.0f, 0.0f };
 }
 
 void Menu::openMenu()
 {
 	menuActive = true;
+}
+void Menu::closeMenu()
+{
+	menuActive = false;
 }
 void Menu::initialise()
 {
@@ -17,19 +22,23 @@ void Menu::initialise()
 	m_startButton.setFillColor(sf::Color::White);
 	m_startButton.setSize(m_startButtonSize);
 	m_startButton.setPosition(m_startButtonPosition);
+
+	if (!m_menuBgTexture.loadFromFile("ASSETS\\IMAGES\\menu_image.png")) {
+		std::cout << "Problem loading bg sprite" << std::endl;
+	}
+	m_menuBgSprite.setTexture(m_menuBgTexture,true);// to reset the dimensions of texture
+	m_menuBgSprite.setPosition(m_menuBgPosition);
 }
 
 void Menu::render(sf::RenderWindow& t_window)
 {
 	if (menuActive) {
+		t_window.draw(m_menuBgSprite);
 		t_window.draw(m_startButton);
 	}
 }
 
-void Menu::setupFontAndText()
-{
-}
-
 void Menu::checkIfPressed(sf::Event t_newEvent, sf::RenderWindow& window)
 {
+	std::cout << "test" << std::endl;
 }
--- RatPack/RatPack/Menu.h
+++ RatPack/RatPack/Menu.h
@@ -4,16 +4,19 @@ class Menu {
 public:
 	Menu();
 	void openMenu();
+	void closeMenu();
 	void initialise();
 	void render(sf::RenderWindow& t_window);
+	void checkIfPressed(sf::Event t_newEvent, sf::RenderWindow& window);
 protected:
 private:
 	bool menuActive;
+	//Menu Sprite
+	sf::Texture m_menuBgTexture;
+	sf::Sprite m_menuBgSprite{ m_menuBgTexture };
+	sf::Vector2f m_menuBgPosition;
 	//Buttons
 	sf::RectangleShape m_startButton;
 	sf::Vector2f m_startButtonSize;
 	sf::Vector2f m_startButtonPosition;
-	//Funcs
-	void setupFontAndText();
-	void checkIfPressed(sf::Event t_newEvent, sf::RenderWindow& window);
 };
