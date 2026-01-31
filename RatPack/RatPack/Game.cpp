4dba8a0cd50e2b47318aa4672b2e7b23c32f4ab3
U-Unknown\User <c00308560@setu.ie>
2026-01-31 10:31:18 +0000

    SFML

---
+++ RatPack/RatPack/Game.cpp
@@ -0,0 +1,124 @@
+#include "Game.h"
+#include <iostream>
+
+Game::Game() :
+	m_window{ sf::VideoMode{ sf::Vector2u{800U, 600U}, 32U }, "SFML Game 3.0" },
+	m_DELETEexitGame{false} //when true game will exit
+{
+	setupTexts(); // load font 
+	setupSprites(); // load texture
+	setupAudio(); // load sounds
+}
+
+Game::~Game()
+{
+}
+
+void Game::run()
+{	
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	const float fps{ 60.0f };
+	sf::Time timePerFrame = sf::seconds(1.0f / fps); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+
+void Game::processEvents()
+{
+	
+	while (const std::optional newEvent = m_window.pollEvent())
+	{
+		if ( newEvent->is<sf::Event::Closed>()) // close window message 
+		{
+			m_DELETEexitGame = true;
+		}
+		if (newEvent->is<sf::Event::KeyPressed>()) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+	}
+}
+
+void Game::processKeys(const std::optional<sf::Event> t_event)
+{
+	const sf::Event::KeyPressed *newKeypress = t_event->getIf<sf::Event::KeyPressed>();
+	if (sf::Keyboard::Key::Escape == newKeypress->code)
+	{
+		m_DELETEexitGame = true; 
+	}
+}
+
+void Game::checkKeyboardState()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+	{
+		m_DELETEexitGame = true; 
+	}
+}
+
+void Game::update(sf::Time t_deltaTime)
+{
+	checkKeyboardState();
+	if (m_DELETEexitGame)
+	{
+		m_window.close();
+	}
+}
+
+void Game::render()
+{
+	m_window.clear(ULTRAMARINE);
+
+	m_window.draw(m_DELETElogoSprite);
+	m_window.draw(m_DELETEwelcomeMessage);
+	
+	m_window.display();
+}
+
+void Game::setupTexts()
+{
+	if (!m_jerseyFont.openFromFile("ASSETS\\FONTS\\Jersey20-Regular.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	m_DELETEwelcomeMessage.setFont(m_jerseyFont);
+	m_DELETEwelcomeMessage.setString("SFML Game");
+	m_DELETEwelcomeMessage.setPosition(sf::Vector2f{ 205.0f, 240.0f });
+	m_DELETEwelcomeMessage.setCharacterSize(96U);
+	m_DELETEwelcomeMessage.setOutlineColor(sf::Color::Black);
+	m_DELETEwelcomeMessage.setFillColor(sf::Color::Red);
+	m_DELETEwelcomeMessage.setOutlineThickness(2.0f);
+
+}
+
+void Game::setupSprites()
+{
+	if (!m_DELETElogoTexture.loadFromFile("ASSETS\\IMAGES\\SFML-LOGO.png"))
+	{
+		// simple error message if previous call fails
+		std::cout << "problem loading logo" << std::endl;
+	}
+	
+	m_DELETElogoSprite.setTexture(m_DELETElogoTexture,true);// to reset the dimensions of texture
+	m_DELETElogoSprite.setPosition(sf::Vector2f{ 150.0f, 50.0f });
+}
+
+void Game::setupAudio()
+{
+	if (!m_DELETEsoundBuffer.loadFromFile("ASSETS\\AUDIO\\beep.wav"))
+	{
+		std::cout << "Error loading beep sound" << std::endl;
+	}
+	m_DELETEsound.play(); // test sound
+}
